a2227e68596b42d709cabc20dd6066b5df4e35c5
gzy02 <1262454489@qq.com>
2022-06-30 05:26:05 +0800

    init

---
+++ client.py
@@ -0,0 +1,196 @@
+import requests
+from typing import Union
+# import httpx
+import json
+import hashlib
+
+
+class client(object):
+    def __init__(self):
+        self.host = "http://121.36.249.52:7777"
+        self.headers = dict()
+
+    def logout(self):
+        req = requests.get(url=self.host + "/api/logout", headers=self.headers)
+        ans = json.loads(req.text)
+        self.headers.pop("Cookie")
+        return ans
+
+    def get_dir_tree(self):
+        req = requests.get(self.host + "/api/share/get_dir_tree", headers=self.headers)
+        ans = json.loads(req.text)
+        return ans
+
+    def get_myinfo(self):
+        req = requests.get(self.host + "/api/myinfo", headers=self.headers)
+        ans = json.loads(req.text)
+        return ans
+
+    def get_identity(self):
+        req = requests.get(self.host + "/api/get_identity", headers=self.headers)
+        ans = json.loads(req.text)
+        return ans
+
+    def get_root_id(self):
+        req = requests.get(self.host + "/api/get_root_id", headers=self.headers)
+        ans = json.loads(req.text)
+        return ans
+
+    def get_dir(self, dir_id: int):
+        req = requests.get(self.host + "/api/filesystem/get_dir", params={"dir_id": dir_id}
+                           , headers=self.headers)
+        ans = json.loads(req.text)
+        return ans
+
+    def get_upload_allocation(self, md5code: str, size: Union[int, None]):
+        params = {"md5": md5code}
+        if size is not None:
+            params["size"] = size
+        req = requests.get(self.host + "/api/upload_allocation", params=params, headers=self.headers)
+        ans = json.loads(req.text)
+        return ans
+
+    def get_file_exist(self, md5code: str):
+        req = requests.get(self.host + "/api/file_exist", params={"md5": md5code}, headers=self.headers)
+        ans = json.loads(req.text)
+        return ans
+
+    def get_file_fragment_exist(self, md5code: str):
+        req = requests.get(self.host + "/api/file_fragment_exist", params={"md5": md5code}, headers=self.headers)
+        ans = json.loads(req.text)
+        return ans
+
+    def download_fragment(self, fdid: int, index: int):
+        req = requests.get(self.host + "/api/download_fragment", params={"fdid": fdid, "index": index},
+                           headers=self.headers)
+        if req.headers["Content-Type"] == "application/json;charset=GBK":
+            ans = json.loads(req.text)
+        else:
+            ans = req.content
+        return ans
+
+    def register(self, user_name: str, password: str):
+        self.headers['content-type'] = 'application/json'
+        req = requests.post(url=self.host + "/api/register", headers=self.headers,
+                            data=json.dumps({"user_name": user_name, "password": password}))
+        ans = json.loads(req.text)
+        return ans
+
+    def login(self, user_name: str, password: str):
+        self.headers['content-type'] = 'application/json'
+        req = requests.post(url=self.host + "/api/login", headers=self.headers,
+                            data=json.dumps({"user_name": user_name, "password": password}))
+        self.headers.pop('content-type')
+        ans = json.loads(req.text)
+        if ans["success"]:
+            self.headers["Cookie"] = (req.headers["Set-Cookie"])
+        return ans
+
+    def create_dir(self, pid: int, dname: str):
+        self.headers['content-type'] = 'application/json'
+        req = requests.post(self.host + "/api/filesystem/create_dir", headers=self.headers
+                            , data=json.dumps({"pid": pid, "dname": dname}))
+        self.headers.pop('content-type')
+        ans = json.loads(req.text)
+        return ans
+
+    def remove_dir(self, dir_id: int):
+        self.headers['content-type'] = 'application/json'
+        req = requests.delete(self.host + "/api/remove_dir", headers=self.headers
+                              , data=json.dumps({"dir_id": dir_id}))
+        self.headers.pop('content-type')
+        ans = json.loads(req.text)
+        return ans
+
+    def remove_file(self, fdid: int):
+        self.headers['content-type'] = 'application/json'
+        req = requests.delete(self.host + "/api/remove_file", headers=self.headers
+                              , data=json.dumps({"fdid": fdid}))
+        self.headers.pop('content-type')
+        ans = json.loads(req.text)
+        return ans
+
+    def rename_dir(self, did: int, dname: str):
+        self.headers['content-type'] = 'application/json'
+        req = requests.put(self.host + "/api/filesystem/rename_dir", headers=self.headers,
+                           data=json.dumps({"did": did, "dname": dname}))
+        self.headers.pop('content-type')
+        ans = json.loads(req.text)
+        return ans
+
+    def rename_file(self, fid: int, fname: str):
+        self.headers['content-type'] = 'application/json'
+        req = requests.put(self.host + "/api/filesystem/rename_file", headers=self.headers,
+                           data=json.dumps({"fid": fid, "fname": fname}))
+        self.headers.pop('content-type')
+        ans = json.loads(req.text)
+        return ans
+
+    def move_file(self, fdid: int, pid: int):
+        self.headers['content-type'] = 'application/json'
+        req = requests.post(self.host + "/api/share/move_file", headers=self.headers,
+                            data=json.dumps({"fdid": fdid, "pid": pid}))
+        self.headers.pop('content-type')
+        ans = json.loads(req.text)
+        return ans
+
+    def copy_file(self, fdid: int, pid: int):
+        self.headers['content-type'] = 'application/json'
+        req = requests.post(self.host + "/api/share/copy_file", headers=self.headers,
+                            data=json.dumps({"fdid": fdid, "pid": pid}))
+        self.headers.pop('content-type')
+        ans = json.loads(req.text)
+        return ans
+
+    def move_dir(self, did: int, pid: int):
+        self.headers['content-type'] = 'application/json'
+        req = requests.post(self.host + "/api/share/move_dir", headers=self.headers,
+                            data=json.dumps({"did": did, "pid": pid}))
+        self.headers.pop('content-type')
+        ans = json.loads(req.text)
+        return ans
+
+    def copy_dir(self, did: int, pid: int):
+        self.headers['content-type'] = 'application/json'
+        req = requests.post(self.host + "/api/share/copy_dir", headers=self.headers,
+                            data=json.dumps({"did": did, "pid": pid}))
+        self.headers.pop('content-type')
+        ans = json.loads(req.text)
+        return ans
+
+    def get_tree(self, did:int):
+        self.headers['content-type'] = 'application/json'
+        req = requests.get(self.host + "/api/filesystem/get_tree", headers=self.headers,
+                           params={"did": did})
+        self.headers.pop('content-type')
+        ans = json.loads(req.text)
+        return ans
+
+    def get_md5code(self, fdid:int):
+        self.headers['content-type'] = 'application/json'
+        req = requests.get(self.host + "/api/filesystem/get_md5code", headers=self.headers,
+                           params={"fdid": fdid})
+        self.headers.pop('content-type')
+        ans = json.loads(req.text)
+        return ans
+
+
+    def upload_file(self, filename: str, parent_dir_id: int, md5code: str):
+        self.headers['content-type'] = 'application/json'
+        body = json.dumps({"filename": filename, "parent_dir_id": parent_dir_id, "md5": md5code}, ensure_ascii=False).encode('utf-8')
+        req = requests.post(self.host + "/api/upload_file", headers=self.headers,
+                            data=body)
+        self.headers.pop('content-type')
+        ans = json.loads(req.text)
+        return ans
+
+    def upload_fragment(self, index: int, file_md5code: str, fragment_md5code: str, fragment=None):
+        if fragment is None:
+            req = requests.post(self.host + "/api/upload_fragment", headers=self.headers,
+                                data={"index": index, "file_md5": file_md5code, "fragment_md5": fragment_md5code})
+        else:
+            req = requests.post(self.host + "/api/upload_fragment", headers=self.headers,
+                                data={"index": index, "file_md5": file_md5code, "fragment_md5": fragment_md5code},
+                                files=[("file_fragment", ("file_fragment", fragment))])
+        ans = json.loads(req.text)
+        return ans
---
+++ downloadThread.py
@@ -0,0 +1,318 @@
+from PyQt6 import QtCore
+from PyQt6.QtCore import QThread, QWaitCondition
+import hashlib
+from client import client
+import chardet
+import time
+import config
+import re
+import os
+import math
+from hashlib import md5
+import shutil
+
+MAX_FRAGMENT_SIZE = 4 * 1024 * 1024
+
+
+class DownloadFileThread(QThread):
+    trigger = QtCore.pyqtSignal(str, str)
+
+    # console = QtCore.pyqtSignal(str)
+
+    def __init__(self):
+        super(DownloadFileThread, self).__init__()
+        self.index = None
+        self.tmp_path = None
+        self.fsize = None
+        self.fdid = None
+        self.save_path = None
+        self.download_index = None
+        self.user_config = None
+        self.current_user = None
+        self.client = None
+        self.filename = None
+        self.isPause = False
+
+    def setOption(self, download_index: str):
+        # , save_path: str, filename: str, fdid: int, fsize: int
+        print("set option")
+        self.client = client()
+        self.current_user = config.get_current_user()
+        self.user_config = config.get_user_config(self.current_user)
+        self.client.headers['Cookie'] = self.user_config['Cookie']
+        self.tmp_path = self.user_config['tmp_path']
+        self.download_index = download_index
+        self.save_path = self.user_config['download_list'][download_index]['save_path']
+        self.filename = self.user_config['download_list'][download_index]['filename']
+        self.fdid = self.user_config['download_list'][download_index]['fdid']
+        self.fsize = self.user_config['download_list'][download_index]['fsize']
+        self.index = self.user_config['download_list'][download_index]['index']
+        self.trigger.connect(self.triggerRecv)
+
+
+    def __del__(self):
+        self.wait()
+
+    def run(self) -> None:
+        try:
+            self.trigger.emit(self.download_index, "start")
+            # self.uploadFile(self.filePath, self.pid)
+            self.downloadFile(self.fdid, self.save_path, self.filename)
+            self.trigger.emit(self.download_index, "success")
+
+        except Exception as e:
+            self.trigger.emit(self.download_index, repr(e))
+        # pass
+
+
+
+    def downloadFile(self, fdid, save_path, filename):
+
+        frag_dir_path = os.path.join(self.tmp_path, str(fdid))
+        if not os.path.exists(frag_dir_path):
+            os.makedirs(frag_dir_path)
+
+        new_path = os.path.join(save_path, filename)
+        if os.path.exists(new_path):
+            ans = self.client.get_md5code(fdid)
+            file_md5 = ans['data']['MD5']
+            new_name = filename
+            hasMD5 = False
+            while True:
+                test_md5 = hashlib.md5(open(os.path.join(save_path, new_name), "rb").read()).hexdigest()
+                if test_md5 == file_md5:
+                    hasMD5 = True
+                    break
+                new_name_vec = new_name.split('.')
+                if len(new_name_vec) > 1:
+                    new_name_vec[-2] += "-副本"
+                    new_name = '.'.join(new_name_vec)
+                else:
+                    new_name += "副本"
+                new_path = os.path.join(save_path, new_name)
+                if not os.path.exists(new_path):
+                    break
+            if hasMD5:
+                return
+
+        max_index = math.ceil(self.fsize / MAX_FRAGMENT_SIZE)
+
+        while self.index < max_index:
+            if self.isPause:
+                time.sleep(0.1)
+                continue
+            self.download_frag(fdid, frag_dir_path, self.index)
+            self.trigger.emit(self.download_index, str(self.index))
+            self.index += 1
+            self.user_config['download_list'][self.download_index]['index'] = self.index
+            config.set_user_config(self.current_user,self.user_config)
+            pass
+        self.combine(filename, save_path, frag_dir_path)
+
+    def triggerRecv(self, download_index, message):
+        if download_index == "-1":
+            self.isPause = not self.isPause
+            print("download isPause:", self.isPause, download_index, message)
+
+    def download_frag(self, fdid, frag_dir_path, index):
+        frag_bits = self.client.download_fragment(fdid, index)
+        if isinstance(frag_bits, dict):
+            print("下载错误", frag_bits)
+        else:
+            print("下载碎片成功")
+            with open(os.path.join(frag_dir_path, str(index)), "wb") as fp:
+                fp.write(frag_bits)
+
+    def combine(self, filename: str, file_save_path: str, file_tmp_path: str):
+        # save_list = os.listdir(file_save_path)
+        # if filename in save_list:
+        #     # 给出一个提示,确认是否覆盖文件
+        #     # 覆盖的话对文件名不进行任何操作
+        #     # 若不覆盖则替换文件名
+        #     pos = filename.rfind(".")
+        #     if pos == -1:
+        #         filename += "_1"
+        #     else:
+        #         filename = filename[:pos] + "_1" + filename[pos:]
+        with open(os.path.join(file_save_path, filename), "wb") as fp:
+            frag_list = os.listdir(file_tmp_path)
+            frag_list.sort(key=lambda x: int(x))
+            for i in frag_list:
+                frag_fp = open(os.path.join(file_tmp_path, str(i)), "rb")
+                fp.write(frag_fp.read())
+                frag_fp.close()
+            shutil.rmtree(file_tmp_path)
+
+
+class DownloadDirThread(QThread):
+    trigger = QtCore.pyqtSignal(str, str)
+
+    # console = QtCore.pyqtSignal(str)
+
+    def __init__(self):
+        super(DownloadDirThread, self).__init__()
+        self.download_task_count = None
+        self.tmp_path = None
+        self.totalSize = None
+        self.save_path = None
+        self.did = None
+        self.user_config = None
+        self.current_user = None
+        self.file_download_list = None
+        self.client = None
+        self.download_index = None
+
+        self.isPause = False
+        self.haveDownload = 0
+
+    def setOption(self, download_index: str):
+        # , did: int, save_path: str
+        print("set option")
+        self.client = client()
+        self.current_user = config.get_current_user()
+        self.user_config = config.get_user_config(self.current_user)
+        self.client.headers['Cookie'] = self.user_config['Cookie']
+        self.tmp_path = self.user_config['tmp_path']
+        self.download_index = download_index
+        self.did = self.user_config['download_list'][self.download_index]['did']
+        self.save_path = self.user_config['download_list'][self.download_index]['save_path']
+        self.haveDownload = self.user_config['download_list'][self.download_index]['haveDownload']
+        self.totalSize = self.user_config['download_list'][self.download_index]['totalSize']
+        self.file_download_list = self.user_config['download_list'][self.download_index]['file_download_list']
+        self.download_task_count = self.user_config['download_list'][self.download_index]['download_task_count']
+        self.trigger.connect(self.triggerRecv)
+
+
+    def __del__(self):
+        self.wait()
+
+    def run(self) -> None:
+        try:
+            if self.file_download_list is None:
+                self.create_all_dir()
+
+            # print("create dir")
+            self.trigger.emit(self.download_index, "start")
+
+            while self.download_task_count < len(self.file_download_list):
+                file_download = self.file_download_list[self.download_task_count]
+                self.downloadFile(file_download)
+                self.haveDownload += file_download['fsize']
+                self.download_task_count += 1
+                self.user_config['download_list'][self.download_index]['download_task_count'] = self.download_task_count
+                self.user_config['download_list'][self.download_index]['haveDownload'] = self.haveDownload
+                config.set_user_config(self.current_user, self.user_config)
+            # print("emit start",self.file_download_list)
+            # for file_download in self.file_download_list:
+            #     # print("download file", file_download['filename'])
+            #     self.downloadFile(file_download)
+            #     self.haveDownload += file_download['fsize']
+            # print("download file end", file_download['filename'])
+            # self.trigger.emit(self.download_index, "file_downloaded")
+            self.trigger.emit(self.download_index, "success")
+
+        except Exception as e:
+            self.trigger.emit(self.download_index, repr(e))
+        # pass
+
+    # def downloadDir(self, dirpath, pid):
+    #
+    #     pass
+    def create_all_dir(self):
+
+        ans = self.client.get_tree(self.did)
+        print(ans)
+
+        tree_list = ans['data']
+        self.file_download_list = []
+
+        self.totalSize = 0
+        self.traverseTreeList(tree_list, self.save_path)
+        print(self.file_download_list)
+        self.user_config['download_list'][self.download_index]['file_download_list'] = self.file_download_list
+        self.user_config['download_list'][self.download_index]['totalSize'] = self.totalSize
+        config.set_user_config(self.current_user, self.user_config)
+
+    def traverseTreeList(self, tree_list, path):
+        for tree in tree_list:
+            if tree['type'] == 0:
+                new_path = os.path.join(path, tree['name'])
+                if os.path.exists(new_path):
+                    ans = self.client.get_md5code(tree['id'])
+                    file_md5 = ans['data']['MD5']
+                    new_name = tree['name']
+                    hasMD5 = False
+                    while True:
+                        test_md5 = hashlib.md5(open(os.path.join(path, new_name), "rb").read()).hexdigest()
+                        if test_md5 == file_md5:
+                            hasMD5 = True
+                            break
+                        new_name_vec = new_name.split('.')
+                        if len(new_name_vec)>1:
+                            new_name_vec[-2] += "-副本"
+                            new_name = '.'.join(new_name_vec)
+                        else:
+                            new_name += "副本"
+                        new_path = os.path.join(path, new_name)
+                        if not os.path.exists(new_path):
+                            break
+                    if hasMD5:
+                        continue
+
+                self.totalSize += tree['size']
+                self.file_download_list.append(
+                    {"fdid": tree['id'], "save_path": new_path, "index": 0,
+                     "fsize": tree['size'], "filename":tree['name']})
+            else:
+                next_path = os.path.join(path, tree['label'])
+                if not os.path.exists(next_path):
+                    os.makedirs(next_path)
+                self.traverseTreeList(tree['children'], next_path)
+
+    def downloadFile(self, file_download):
+        save_path = file_download['save_path']
+        fdid = file_download['fdid']
+        fsize = file_download['fsize']
+        filename = file_download['filename']
+        frag_dir_path = os.path.join(self.tmp_path, str(fdid))
+        index = file_download['index']
+        if not os.path.exists(frag_dir_path):
+            os.makedirs(frag_dir_path)
+        max_index = math.ceil(fsize / MAX_FRAGMENT_SIZE)
+        while index < max_index:
+            if self.isPause:
+                time.sleep(0.1)
+                continue
+            self.download_frag(fdid, frag_dir_path, index)
+            self.trigger.emit(self.download_index, str(self.haveDownload+min((index+1)*MAX_FRAGMENT_SIZE,fsize)))
+            index += 1
+            file_download['index'] = index
+            config.set_user_config(self.current_user,self.user_config)
+            pass
+        self.combine(save_path, frag_dir_path)
+
+    def triggerRecv(self, download_index, message):
+        if download_index == "-1":
+            self.isPause = not self.isPause
+            print("download isPause:", self.isPause, download_index, message)
+
+    def download_frag(self, fdid, frag_dir_path, index):
+        frag_bits = self.client.download_fragment(fdid, index)
+        if isinstance(frag_bits, dict):
+            print("下载错误", frag_bits)
+        else:
+            print("下载碎片成功")
+            with open(os.path.join(frag_dir_path, str(index)), "wb") as fp:
+                fp.write(frag_bits)
+
+    def combine(self, file_save_path: str, file_tmp_path: str):
+        with open(file_save_path, "wb") as fp:
+            frag_list = os.listdir(file_tmp_path)
+            frag_list.sort(key=lambda x: int(x))
+            for i in frag_list:
+                frag_fp = open(os.path.join(file_tmp_path, str(i)), "rb")
+                fp.write(frag_fp.read())
+                frag_fp.close()
+            shutil.rmtree(file_tmp_path)
+
+
